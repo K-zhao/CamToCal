83f12737e3ed9d38a8b930664b288dc2fdca0c49
K-zhao <kevzhao2@gmail.com>
2020-12-31 16:27:49 -0800

    Implemented text recognition of photo.

--- app/src/main/java/com/example/CamToCal/ui/Display.java
+++ app/src/main/java/com/example/CamToCal/ui/Display.java
@@ -1,15 +1,27 @@
 package com.example.CamToCal.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.CamToCal.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+
+import java.util.List;
 
 public class Display extends AppCompatActivity {
+
+    private Bitmap picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +29,40 @@ public class Display extends AppCompatActivity {
         setContentView(R.layout.activity_display);
         ImageView imageView = findViewById(R.id.imageView2);
 
-        Bitmap bitmap = BitmapFactory.decodeFile(getIntent().getStringExtra("image_path"));
-        imageView.setImageBitmap(bitmap);
+        picture = BitmapFactory.decodeFile(getIntent().getStringExtra("image_path"));
+        imageView.setImageBitmap(picture);
+        runTextRecognition();
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    private void runTextRecognition() {
+        InputImage image = InputImage.fromBitmap(picture, 0);
+        TextRecognizer recognizer = TextRecognition.getClient();
+        recognizer.process(image)
+                .addOnSuccessListener(
+                        texts -> processTextRecognitionResult(texts))
+                .addOnFailureListener(
+                        e -> {
+                            // Task failed with an exception
+                            e.printStackTrace();
+                        });
+    }
+
+    private void processTextRecognitionResult(Text texts) {
+        String sentence = "";
+        List<Text.TextBlock> blocks = texts.getTextBlocks();
+        if (blocks.size() == 0) {
+            showToast("No text found");
+            return;
+        } else {
+            // Join up text found in photo into a string
+            for (Text.TextBlock text: blocks) {
+                sentence += " " + text.getText();
+            }
+            showToast(sentence);
+        }
     }
 }
